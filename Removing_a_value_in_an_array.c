412110dd5436550f9008373efe5c33f78c936ce0
zamirul <javshazib@gmail.com>
2026-04-20 22:42:53 +0600

    Removing_a_value_in_an_array

--- Removing_a_value_in_an_array.c
+++ Removing_a_value_in_an_array.c
@@ -11,5 +11,15 @@ int main()
     int indx;
     scanf("%d", &indx);
 
+    for (int i = indx; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    n--;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
